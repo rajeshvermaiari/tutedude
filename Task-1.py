c730511b8101d92bf27c6209335bc0318b8e551f
rajeshvermaiari <rajeshvermaiari@gmail.com>
2025-04-17 12:01:28 +0530

    Add files via upload

---
+++ Task-1.py
@@ -0,0 +1,8 @@
+#Task-1
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+
+print("Addition:" , int(num1) + int(num2))
+print("Substraction:" , int(num1) - int(num2))
+print("Multiplication:" , int(num1) * int(num2))
+print("Division:" , int(num1) / int(num2))
---
+++ Task-2.py
@@ -0,0 +1,5 @@
+#Task-2
+fname = input("Enter your first name:")
+lname = input("Enter your second name:")
+
+print("Hellow," ,fname,lname,'!',"Welcome to python program")
